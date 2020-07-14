6585f2b6afc0fac761be551aac5b665682b277de
Martin <martinthelindberg@gmail.com>
2020-07-14 14:30:14 -0700

    lables for sections

--- components/PickSection.vue
+++ components/PickSection.vue
@@ -1,20 +1,26 @@
 <template>
   <div>
     <div class="toolbar border yellow">
-      Pick a section to start drawing!
+      pick a section to start drawing!
     </div>
     <div
       class="section-template border pointer"
       @click="$emit('picked-type', 'top')"
-    ></div>
+    >
+      <div class="meta-box"><div class="info">top</div></div>
+    </div>
     <div
       class="section-template border pointer"
       @click="$emit('picked-type', 'mid')"
-    ></div>
+    >
+      <div class="meta-box"><div class="info">middle</div></div>
+    </div>
     <div
       class="section-template border pointer"
       @click="$emit('picked-type', 'bot')"
-    ></div>
+    >
+      <div class="meta-box"><div class="info">bottom</div></div>
+    </div>
   </div>
 </template>
 
@@ -33,6 +39,7 @@ export default {
 }
 
 .section-template {
+  position: relative;
   width: 544px;
   height: 180px;
   margin: calc(40px / 3) 0;
@@ -44,6 +51,40 @@ export default {
     --box-shadow-color: var(--orange);
     --box-shadow-size: 6px;
     transform: translate3d(-4px, -4px, 0);
+
+    .meta-box {
+      display: block;
+    }
+  }
+}
+
+.meta-box {
+  display: none;
+  position: absolute;
+  z-index: 100;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  // http://www.patternify.com/
+  background: transparent
+    url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAkAAAAJCAYAAADgkQYQAAAAFUlEQVQYlWNgoAb4N4HhP2UKhoUVAL3oD0/YmVPIAAAAAElFTkSuQmCC)
+    repeat;
+
+  .info {
+    position: absolute;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    z-index: 101;
+    top: 50%;
+    left: 50%;
+    transform: translate3d(-50%, -50%, 0);
+    background: var(--white);
+    height: 60px;
+    min-width: 33%;
+    border: 2px solid var(--orange);
+    padding: 0 1rem;
   }
 }
 </style>
