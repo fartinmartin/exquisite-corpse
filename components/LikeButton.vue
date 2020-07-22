f56198f0e3fb0459b8a28ce4d07227300311a20d
Martin <martinthelindberg@gmail.com>
2020-07-22 10:29:05 -0700

    begin like button

---
+++ components/LikeButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <button @click="handleClick">
+    <div class="icon interactive"><span>0</span> ❤️</div>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "LikeButton",
+  props: {
+    collection: {
+      type: String,
+      required: true
+    },
+    id: {
+      type: String,
+      required: true
+    }
+  },
+  data: function() {
+    return {
+      likes: 0,
+      isLiked: false
+    };
+  },
+  mounted() {},
+  methods: {
+    handleClick() {}
+  }
+};
+</script>
+
+<style></style>
