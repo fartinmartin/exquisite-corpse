27162b8b77a7d6145624a6a936949d811bcd4a07
Martin <martinthelindberg@gmail.com>
2020-07-26 14:06:56 -0700

    start to caching drawings in store from gallery.. not sure if this is a great idea

--- store/modules/gallery.js
+++ store/modules/gallery.js
@@ -1,7 +1,29 @@
-export const state = () => ({});
+export const state = () => ({
+  completed: {},
+  sections: {},
+});
 
-export const getters = {};
+export const getters = {
+  getDocById: (state) => (docId) => {
+    return state.completed[docId] || state.sections[docId];
+  },
+};
 
-export const actions = {};
+export const actions = {
+  pushDocToStore({ commit }, doc) {
+    if (doc.collection === "completed") {
+      commit("PUSH_COMPLETED_TO_STORE", doc);
+    } else {
+      commit("PUSH_SECTOION_TO_STORE", doc);
+    }
+  },
+};
 
-export const mutations = {};
+export const mutations = {
+  PUSH_COMPLETED_TO_STORE(state, doc) {
+    state.completed[doc.docId] = doc;
+  },
+  PUSH_SECTION_TO_STORE(state, doc) {
+    state.sections[doc.docId] = doc;
+  },
+};
