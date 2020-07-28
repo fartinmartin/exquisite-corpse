c8d0f836c522170d817a7ec57d12171c887929ae
Martin <martinthelindberg@gmail.com>
2020-07-28 13:06:49 -0700

    refreactor console css

--- store/modules/user.js
+++ store/modules/user.js
@@ -7,26 +7,19 @@ export const getters = {};
 
 export const actions = {
   async signOut() {
-    const style = [
-      "display: inline-block;",
-      "text-align: center",
-      "font-family: 'Space Mono', monospace;",
-      "font-size: 12px;",
-      "word-spacing: 1px;",
-      "padding: 1rem;",
-      "margin: 20px auto 22px auto;",
+    const errStyle = [
+      ...style,
       "border: 2px solid #fb9200;",
       "box-shadow: 2px 2px 0 0 #f44e3b;",
-      "background: #ffffff;",
     ];
 
     this.$fireAuth
       .signOut()
       .then(() => {
-        console.log("%c👋 signed out", style.join(""));
+        console.log("%c👋 signed out", errStyle.join(""));
       })
       .catch((error) => {
-        console.log("%c🚫 sign out error", style.join(""));
+        console.log("%c🚫 sign out error", errStyle.join(""));
         console.error(error);
       });
   },
@@ -82,18 +75,6 @@ export const actions = {
     return words;
   },
   welcomeUser() {
-    const style = [
-      "display: inline-block;",
-      "text-align: center",
-      "font-family: 'Space Mono', monospace;",
-      "font-size: 12px;",
-      "word-spacing: 1px;",
-      "padding: 1rem;",
-      "margin: 20px auto 22px auto;",
-      "border: 2px solid #fcda00;",
-      "box-shadow: 2px 2px 0 0 #fb9200;",
-      "background: #ffffff;",
-    ];
     console.log(
       `%c🎨 welcome ${
         this.$fireAuth.currentUser.displayName || this.$fireAuth.currentUser.id
@@ -105,8 +86,20 @@ export const actions = {
 
 export const mutations = {
   SET_USER(state, user) {
-    // state.name = user.displayName;
     state.name = user.displayName || `anonymous-${user.uid.substr(1, 4)}`;
     state.id = user.uid;
   },
 };
+
+const style = [
+  "display: inline-block;",
+  "text-align: center",
+  "font-family: 'Space Mono', monospace;",
+  "font-size: 12px;",
+  "word-spacing: 1px;",
+  "padding: 1rem;",
+  "margin: 20px auto 22px auto;",
+  "border: 2px solid #fcda00;",
+  "box-shadow: 2px 2px 0 0 #fb9200;",
+  "background: #ffffff;",
+];
