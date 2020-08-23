007bd55a2df5e44a05f93f212507b250b8fadf02
Martin <martinthelindberg@gmail.com>
2020-08-22 18:44:19 -0700

    start to Icon.vue

--- components/TODO/Icon.vue
+++ components/TODO/Icon.vue
@@ -1,7 +1,86 @@
-<template> </template>
+<template>
+  <div class="icon interactive">
+    <slot />
+  </div>
+</template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.icon {
+  font-size: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 25px;
+  height: 25px;
+
+  box-sizing: content-box;
+  border: 2px solid transparent;
+
+  &.interactive:active {
+    border: 2px solid var(--lighter-yellow);
+    border-top: 2px solid var(--yellow);
+    border-left: 2px solid var(--yellow);
+    transform: translate3d(2px, 2px, 0);
+  }
+
+  span,
+  img {
+    pointer-events: none;
+  }
+}
+
+.look-at-me {
+  --animation-delay: 250ms;
+  position: relative;
+  overflow: hidden;
+
+  &::before,
+  &:after {
+    content: "";
+    position: absolute;
+    top: 25%;
+    left: 50%;
+    height: 100%;
+    transform: rotate(45deg) translateX(-30px);
+    background: rgba(255, 255, 255, 0.45);
+    mix-blend-mode: screen;
+    pointer-events: none;
+
+    animation: diagonal-shimmer 2.5s infinite ease-in-out;
+  }
+
+  &::before {
+    width: 4px;
+    animation-delay: calc(var(--animation-delay) + 0.25s);
+    background: rgba(255, 255, 255, 0.25);
+  }
+
+  &::after {
+    width: 10px;
+    animation-delay: calc(var(--animation-delay));
+  }
+
+  /* @for $i from 1 through 2 {
+    &:nth-child(#{$i}) {
+      --animation-delay: 250ms * #{$i};
+    }
+  } */
+}
+
+@keyframes diagonal-shimmer {
+  0% {
+    transform: rotate(45deg) translateX(-30px);
+  }
+  50% {
+    transform: rotate(50deg) translateX(10px);
+  }
+  100% {
+    transform: rotate(50deg) translateX(10px);
+  }
+}
+</style>
