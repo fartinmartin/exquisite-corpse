dbf180bc467b274889d06928b882ee3a6c6ddd83
Martin <martinthelindberg@gmail.com>
2020-09-10 17:31:55 -0700

    fix button!

--- components/PrevNext.vue
+++ components/PrevNext.vue
@@ -1,35 +1,38 @@
 <template>
   <Panel class="prev-next mt" :class="type">
-    <div class="prev">
-      <button
-        :disabled="isFirstPage"
-        @click="$emit('prev')"
-        :data-tooltip="isFirstPage ? `this is the first page! :)` : `go back`"
-      >
-        <div class="icon interactive">
+    <button
+      class="prev"
+      :disabled="isFirstPage"
+      @click="$emit('prev')"
+      :data-tooltip="isFirstPage ? `this is the first page! :)` : `go back`"
+    >
+      <div class="icon interactive">
+        <div class="icon">
           <img src="~/assets/img/icons/arrow.svg" alt="" />
         </div>
-        back
-      </button>
-    </div>
-    <div class="next">
-      <button
-        :disabled="isLastPage"
-        @click="$emit('next')"
-        :data-tooltip="
-          isLastPage ? `no more ${viewing} :(` : `more ${viewing}!`
-        "
-      >
-        more!
-        <div class="icon interactive">
+        <span>back</span>
+      </div>
+    </button>
+
+    <button
+      class="next"
+      :disabled="isLastPage"
+      @click="$emit('next')"
+      :data-tooltip="
+        isLastPage ? `no more ${currentType} :(` : `more ${currentType}!`
+      "
+    >
+      <div class="icon interactive">
+        <span>more!</span>
+        <div class="icon">
           <img
             src="~/assets/img/icons/arrow.svg"
             alt=""
             style="transform: rotate(180deg)"
           />
         </div>
-      </button>
-    </div>
+      </div>
+    </button>
   </Panel>
 </template>
 
@@ -38,8 +41,7 @@ export default {
   name: "PrevNext",
   props: { isFirstPage: Boolean, isLastPage: Boolean, type: String },
   computed: {
-    viewing() {
-      console.log(this.type);
+    currentType() {
       switch (this.type) {
         case "bot":
           return `bottoms`;
@@ -62,17 +64,27 @@ export default {
 <style lang="scss">
 .prev-next .content {
   display: flex;
-  /* justify-content: center; */
   justify-content: space-between !important;
+  height: 25px;
 }
 
-.prev-next button {
+.prev-next button .interactive {
   display: flex;
+  width: auto;
   height: 25px;
   align-items: center;
 }
 
+/* 🚨 TODO: could tweak loading component to match new height too */
 .prev-next:not(.corpses) {
   transform: translateY(calc(calc(40px / 3) + 56.15px * -1));
+}
+
+.prev span {
+  margin-right: 6px;
+}
+
+.next span {
+  margin-left: 6px;
 }
 </style>
