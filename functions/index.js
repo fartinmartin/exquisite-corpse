384f7685ab17076cb12b0b19f75b3b7a8d730bf0
fartinmartin <martinthelindberg@gmail.com>
2020-11-01 11:33:02 -0800

    set cache to 1 week (browser) and 2 weeks (cdn)

--- functions/index.js
+++ functions/index.js
@@ -14,7 +14,16 @@ const config = {
 const nuxt = new Nuxt(config);
 
 exports.ssrapp = functions.https.onRequest(async (req, res) => {
-  res.set("Cache-Control", "public, max-age=300, s-maxage=600"); // 5min & 10min
+  // ⚠️ how long can we afford to go?
+  //
+  // since this cache setting is only controlling the caching of the front-end
+  // and the drawing data is being loaded client side via JS... could it be 31536000 (a year)??
+  //
+  // i guess it depends on how often i will be updating this project, right?
+  //
+  // or is the firebase hosting CDN smart enough to know content has changed? 🤔
+  // https://www.youtube.com/watch?v=LOeioOKUKI8
+  res.set("Cache-Control", "public, max-age=604800, s-maxage=1209600"); // 604800s = 1 week
   await nuxt.ready();
   nuxt.render(req, res);
 });
